14f8725be5b1cfe308b3beadc97a219c798893c1
iris22341 <iris22341@gmail.com>
2026-05-08 14:05:42 +0800

    Update mtsyue_DiAriEs.php

--- post/mtsyue_DiAriEs.php
+++ post/mtsyue_DiAriEs.php
@@ -165,7 +165,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit_button'])) {
                         </tr>
                         <tr class="column-header">
                             <th style="width: 20%;">時間</th>
-                            <th">地點</th>
+                            <th>地點</th>
                             <th style="width: 20%;">時間</th>
                             <th>地點</th>
                         </tr>
